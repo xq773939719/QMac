64fa5ee656d3deb688dc78374da0bdbcbbfa4c3c
XQ <773939719@qq.com>
2021-02-17 19:53:09 +0800

    init

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by XQ on 2021/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding(.bottom)
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Shared/QMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  QMacApp.swift
+//  Shared
+//
+//  Created by XQ on 2021/2/17.
+//
+
+import SwiftUI
+
+@main
+struct QMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
